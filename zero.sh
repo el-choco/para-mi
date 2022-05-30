81d871f5e488b461cbd6cc4eb956b8ac189ec31f
Carsten Rieger IT-Services <criegerde@noreply.codeberg.org>
2022-05-30 08:16:45 +0200

    zero.sh

--- zero.sh
+++ zero.sh
@@ -2,6 +2,7 @@
 ##########################################################################################
 # Ubuntu 20.04+ / 22.04+ LTS x86_64 / Debian 11 x86_64
 # Nextcloud latest or older Versions
+# Based on nginx, PHP, MariaDB/postgreSQL, Redis, fail2ban, ufw
 # Carsten Rieger IT-Services (www.c-rieger.de)
 ##########################################################################################
 
