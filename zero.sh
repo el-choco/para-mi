770ee507fbeb93b1b8b04a97c04921a7f31255c7
Carsten Rieger IT-Services <criegerde@noreply.codeberg.org>
2022-06-06 16:34:28 +0200

    zero.sh
    aufgeräumt

--- zero.sh
+++ zero.sh
@@ -455,7 +455,7 @@ ${echo} ""
 sleep 3
 ${apt} upgrade -y
 ${apt} install -y \
-apt-transport-https bash-completion bzip2 ca-certificates cron curl dialog dirmngr ffmpeg ghostscript git gpg gnupg gnupg2 htop jq \
+apt-transport-https bash-completion bzip2 ca-certificates cron curl dialog dirmngr ffmpeg ghostscript gpg gnupg gnupg2 htop jq \
 libfile-fcntllock-perl libfontconfig1 libfuse2 locate net-tools rsyslog screen smbclient socat software-properties-common \
 ssl-cert tree unzip wget zip
 if [ "$(lsb_release -r | awk '{ print $2 }')" = "20.04" ] || [ "$(lsb_release -r | awk '{ print $2 }')" = "22.04" ]
