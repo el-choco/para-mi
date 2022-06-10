ab0c91eacff11b1f54d70ef070502a4eb9ee7f6f
Carsten Rieger IT-Services <info@c-rieger.de>
2022-06-10 12:16:52 +0200

    zero.sh information. Closes #6, closes #9, closes #11.

--- zero.sh
+++ zero.sh
@@ -3,7 +3,7 @@
 # Ubuntu 20.04+ / 22.04+ LTS x86_64 (recommended!) / Debian 11 x86_64
 # Nextcloud latest (or older Versions)
 # Based on nginx, PHP, MariaDB/postgreSQL, Redis, fail2ban, ufw
-# Carsten Rieger IT-Services (www.c-rieger.de)
+# Carsten Rieger IT-Services (https://www.c-rieger.de)
 ##########################################################################################
 
 ##############################
