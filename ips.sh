e65edb3c75d212728d5d99d0246072da0e05a478
Carsten Rieger IT-Services <info@c-rieger.de>
2023-09-22 11:19:13 +0200

    IPS CROWDSEC

---
+++ ips.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+##########################################################################################
+# CROWDSEC INSTALLATION
+##########################################################################################
+# Debian 12 / Ubuntu 22.04+ LTS x86_64
+# Carsten Rieger IT-Services (https://www.c-rieger.de)
+##########################################################################################
+clear
+echo ""
+echo " » fail2ban wird entfernt  // remove fail2ban"
+echo ""
+sleep 2
+systemctl stop fail2ban.service
+systemctl disable fail2ban.service
+systemctl mask fail2ban.service
+apt remove fail2ban --purge -y
+clear
+echo ""
+echo " » fail2ban entfernt // removed           [OK]"
+echo " » Crowdsec wird heruntergeladen+installiert // crowdsec will be downloaded+installed"
+echo ""
+sleep 2
+curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
+apt install crowdsec -y
+apt install crowdsec-firewall-bouncer-nftables -y
+systemctl enable --now crowdsec.service
+cscli collections install crowdsecurity/nextcloud
+cscli collections install crowdsecurity/nginx
+cscli collections install crowdsecurity/sshd
+systemctl reload crowdsec && systemctl restart crowdsec
+cp /etc/crowdsec/acquis.yaml /etc/crowdsec/acquis.yaml.bak
+cat <<EOF >>/etc/crowdsec/acquis.yaml
+#Nextcloud by c-rieger.de
+filenames:
+ - /var/log/nextcloud/nextcloud.log
+labels:
+  type: Nextcloud
+---
+EOF
+systemctl reload crowdsec && systemctl restart crowdsecsada
+clear
+echo ""
+echo " » fail2ban entfernt // removed           [OK]"
+echo " » crowdsec installiert // installed      [OK]"
+echo " » https://www.c-rieger.de"
+echo ""
+exit 0
