68c30d8247181f0d1f2d9e4ab99622e9ed726503
Carsten Rieger IT-Services <info@c-rieger.de>
2024-06-20 12:51:30 +0200

    repository pinning to prefer nginx packages

--- debian.sh
+++ debian.sh
@@ -407,6 +407,7 @@ ${wget} -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 ###########################
 ${curl} https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" | sudo tee /etc/apt/sources.list.d/nginx.list
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | sudo tee /etc/apt/preferences.d/99nginx
 ###########################
 # DB Repositories         #
 ###########################
