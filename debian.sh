4790f8625a20cebee2de78b469b8253e9b7c3522
criegerde <info@c-rieger.de>
2025-04-20 18:55:22 +0200

    Debian 12

--- debian.sh
+++ debian.sh
@@ -417,21 +417,21 @@ echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 
 ###########################
 # DB Repositories         #
 ###########################
-if [ $DATABASE == "m" ] then
-    ${cat} <<EOF >/etc/apt/sources.list.d/mariadb.sources
-    # MariaDB 11.4 repository list - created 2023-11-20 07:47 UTC
-    # https://mariadb.org/download/
-    X-Repolib-Name: MariaDB
-    Types: deb
-    # deb.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
-    # URIs: https://deb.mariadb.org/11.1/debian
-    URIs: https://mirrors.aliyun.com/mariadb/repo/11.4/debian
-    Suites: bookworm
-    Components: main
-    Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
-    EOF
-    ${curl} -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
-    fi
+if [ $DATABASE == "m" ]
+then
+cat <<EOF >/etc/apt/sources.list.d/mariadb.sources
+# MariaDB 11.4 repository list - created 2023-11-20 07:47 UTC
+# https://mariadb.org/download/
+X-Repolib-Name: MariaDB
+Types: deb
+# deb.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+# URIs: https://deb.mariadb.org/11.1/debian
+URIs: https://mirrors.aliyun.com/mariadb/repo/11.4/debian
+Suites: bookworm
+Components: main
+Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
+EOF
+curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
 else
 	${curl} -fsSl https://www.postgresql.org/media/keys/ACCC4CF8.asc | gpg --dearmor | sudo tee /usr/share/keyrings/postgresql.gpg > /dev/null
 	echo deb [signed-by=/usr/share/keyrings/postgresql.gpg] http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main | sudo tee /etc/apt/sources.list.d/postgresql.list        
