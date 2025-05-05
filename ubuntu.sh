21bc46bfc3ad5b95f54e1b792f3dfe6bf2bffe87
criegerde <info@c-rieger.de>
2025-05-05 06:58:37 +0200

    Redis-Server v. 8 OpenSource

--- ubuntu.sh
+++ ubuntu.sh
@@ -756,9 +756,9 @@ ${apt} update
 ${apt} install -y redis-server redis-tools --allow-change-held-packages
 ${cp} /etc/redis/redis.conf /etc/redis/redis.conf.bak
 ${sed} -i 's/port 6379/port 0/' /etc/redis/redis.conf
-${sed} -i 's/\# unixsocket\ .*\$/unixsocket \/var\/run\/redis\/redis-server.sock/' /etc/redis/redis.conf
-${sed} -i 's/\#\ unixsocketperm\ .*\$/unixsocketperm 770/g' /etc/redis/redis.conf
-${sed} -i 's/# maxclients\ .*\$/maxclients 10240/' /etc/redis/redis.conf
+${sed} -i 's/\# unixsocket /run/redis.sock/unixsocket \/var\/run\/redis\/redis-server.sock/' /etc/redis/redis.conf
+${sed} -i 's/\#\ unixsocketperm 770\$/unixsocketperm 770/' /etc/redis/redis.conf
+${sed} -i 's/# maxclients 10000/maxclients 10240/' /etc/redis/redis.conf
 ${sed} -i "s/# requirepass foobared/requirepass $REDISPASSWORD/" /etc/redis/redis.conf
 ${cp} /etc/sysctl.conf /etc/sysctl.conf.bak
 ${sed} -i '$avm.overcommit_memory = 1' /etc/sysctl.conf
